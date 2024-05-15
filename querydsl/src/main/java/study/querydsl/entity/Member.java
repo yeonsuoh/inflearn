c68f155c406a681560e92026e969c8f7cc3b8ae3
lilylemonoh <lilylemonoh@naver.com>
2024-05-16 05:21:59 +0900

    섹션2 예제 도메인 모델 - 예제 도메인 모델과 동작 확인

---
+++ querydsl/src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,51 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 막고 싶은데, JPA 스펙상 PROTECTED로 열어두어야 함
+@ToString(of = {"id", "username", "age"}) // ToString은 가급적 내부 필드만(연관관계 없는 필드만)
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) {
+            changeTeam(team);
+        }
+    }
+
+    // changeTeam()으로 양방향 연관관계 한번에 처리(연관관계 편의 메소드)
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+}
