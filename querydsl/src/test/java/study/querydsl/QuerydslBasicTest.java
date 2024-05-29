7ee298345abc1caa12059c4794f1b9a805ddb412
lilylemonoh <lilylemonoh@naver.com>
2024-05-30 06:14:33 +0900

    섹션3 기본 문법 - 조인 - 페치 조인

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -366,6 +366,42 @@ public class QuerydslBasicTest {
         }
     }
 
+    // 조인 - 페치 조인
+    // 페치 조인 미적용 - 지연 로딩으로 Member, Team SQL 쿼리 각각 실행
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
+    @Test
+    public void fetchJoinNo() throws Exception{
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 미적용").isFalse();
+    }
+
+    // 페치 조인 적용
+    // 즉시 로딩으로 Member, Team SQL 쿼리 조인으로 한번에 조회
+    @Test
+    public void fetchJoinUse() throws Exception {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 적용").isTrue();
+    }
+
 
 
 
