678d6a6163a016c8373a472507c1aefce337b9f1
lilylemonoh <lilylemonoh@naver.com>
2024-05-29 20:02:49 +0900

    섹션3 기본 문법 - 조인 - on절

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -318,5 +318,55 @@ public class QuerydslBasicTest {
                 .containsExactly("teamA", "teamB");
     }
 
+    // 조인 - on절
+    // 1. 조인 대상 필터링
+    // 2. 연관관계 없는 엔티티 외부 조인
+
+    /**
+     * 예) 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: SELECT m, t FROM Member m LEFT JOIN m.team t on t.name = 'teamA'
+     * SQL: SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.TEAM_ID = t.id and t.name = 'teamA'
+     */
+    @Test
+    public void join_on_filtering() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team).on(team.name.eq("teamA"))
+                // inner 조인일 경우 on절 말고 where 절로 걸러도 결과가 같다.
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+
+    // 2. 연관관계 없는 엔티티 외부 조인
+    // 예) 회원의 이름과 팀의 이름이 같은 대상 외부 조인
+    /**
+     * 2. 연관관계 없는 엔티티 외부 조인
+     * 예) 회원의 이름과 팀의 이름이 같은 대상 외부 조인
+     * JPQL: SELECT m, t FROM Member m LEFT JOIN Team t on m.username = t.name
+     * SQL: SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.username = t.name
+     */
+    @Test
+    public void join_on_no_relation() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("t =" + tuple);
+        }
+    }
+
+
+
 
 }
