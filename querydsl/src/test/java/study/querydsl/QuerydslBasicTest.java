89944f22d3f62088f7b6f0335b3531cdb0126b91
lilylemonoh <lilylemonoh@naver.com>
2024-06-03 05:50:48 +0900

    섹션4 중급 문법 - 프로젝션과 결과 반환 - 기본

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -549,4 +549,41 @@ public class QuerydslBasicTest {
         }
     }
 
+    //--------------------------------------------4. 중급 문법 ----------------------------------------------
+    // 프로젝션과 결과 반환 - 기본
+    // 프로젝션: select 대상 지정
+    // 프로젝션 대상이 하나
+    @Test
+    public void simpleProjection() throws Exception {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+    // 프로젝션 대상이 하나면 타입을 명확하게 지정할 수 있음
+    // 프로젝션 대상이 둘 이상이면 튜플이나 DTO로 조회
+
+    // 튜플 조회
+    // 프로젝션 대상이 둘 이상일 때 사용
+    @Test
+    public void tupleProjection() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username=" + username);
+            System.out.println("age = " + age);
+        }
+    }
+    //=== 프로젝션과 결과 반환 - DTO 조회
+    // 클래스 분리..
+
+
 }
