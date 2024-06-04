4f0e2e0fbbb4d0c374df1ed6a00b2dc1ff12689d
lilylemonoh <lilylemonoh@naver.com>
2024-06-05 06:06:33 +0900

    섹션4 중급 문법 - 프로젝션과 결과 반환 - @QueryProjection

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -691,5 +692,21 @@ public class QuerydslBasicTest {
         }
     }
 
+    //---------- 프로젝션과 결과 반환 - @QueryProjection
+    // 생성자 + @QueryProjection
+
+    // @QueryProjection 활용
+    @Test
+    public void findUDtoByQueryProjection() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+        // 이 방법은 컴파일러로 타입을 체크할 수 있으므로 가장 안전한 방법이다.
+        // 다만 DTO에 QueryDSL 어노테이션을 유지해야 하는 점과 DTO까지 Q파일을 생성해야 하는 단점이 있다.
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
 
 }
