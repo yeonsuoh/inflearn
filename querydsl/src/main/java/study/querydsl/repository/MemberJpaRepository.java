0bc09e062cb2ec824dcdf1015121c64d6a258966
lilylemonoh <lilylemonoh@naver.com>
2024-06-09 13:34:45 +0900

    섹션5 실무 활용 - 순수 JPA와 Querydsl

---
+++ querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -0,0 +1,61 @@
+package study.querydsl.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+import static study.querydsl.entity.QMember.member;
+
+@Repository
+public class MemberJpaRepository {
+
+    private final EntityManager em;
+    private final JPAQueryFactory queryFactory;
+
+    public MemberJpaRepository(EntityManager em) {
+        this.em = em;
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+    // 여기서 JPAQueryFactory는 스프링 빈으로 등록해서 인젝션 받는 형식(RequiredArgument)으로 사용할 수도 있다!
+
+    // 순수 JPA 리포지토리
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member findMember = em.find(Member.class, id);
+        return Optional.ofNullable(findMember);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByUsername(String username) {
+        return em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", username)
+                .getResultList();
+    }
+
+    // -------- Querydsl 사용
+    // 순수 JPA 리포지토리 - Querydsl 추가
+    public List<Member> findAll_Querydsl() {
+        return queryFactory
+                .selectFrom(member).fetch();
+    }
+
+    public List<Member> findByUsername_Querydsl(String username) {
+        return queryFactory
+                .selectFrom(member)
+                .where(member.username.eq(username))
+                .fetch();
+    }
+
+}
