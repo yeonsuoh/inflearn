2e3a07cba5685e4c9e4a653e16e0af4afae4fd2b
lilylemonoh <lilylemonoh@naver.com>
2024-06-14 22:55:10 +0900

    섹션6 실무 활용 - 스프링 데이터 JPA와 Querydsl - 사용자 정의 리포지토리

---
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryCustom.java
@@ -0,0 +1,13 @@
+package study.querydsl.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+
+import java.util.List;
+
+public interface MemberRepositoryCustom {
+
+    List<MemberTeamDto> search(MemberSearchCondition condition);
+}
