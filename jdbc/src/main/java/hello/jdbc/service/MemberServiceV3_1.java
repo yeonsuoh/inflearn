b79fd6de7c0d05465aaec1369e347044e8cb1dc6
lilylemonoh <lilylemonoh@naver.com>
2024-07-20 20:47:51 +0900

    섹션 4 스프링과 문제 해결 - 트랜잭션 - 트랜잭션 문제 해결 - 트랜잭션 매니저1

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,72 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV2;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@RequiredArgsConstructor
+@Slf4j
+public class MemberServiceV3_1 {
+
+//    private final DataSource dataSource;
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+//        Connection con = dataSource.getConnection();
+
+        // 트랜잭션 시작
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            // 비즈니스 로직
+            bizLogic(fromId, toId, money);
+            // 커밋
+            transactionManager.commit(status); // 성공시 커밋
+        } catch (Exception e) {
+            transactionManager.rollback(status); // 실패시 롤백
+            throw new IllegalStateException(e);
+        }
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+    private static void release(Connection con) {
+        if (con != null) {
+            try {
+                con.setAutoCommit(true); // 커넥션 풀 고려
+                // 커넥션 풀을 사용하면 커넥션이 종료되는 것이 아니라 풀에 반납됨
+                // 현재 수동 커밋 모드로 동작하기 때문에 돌려주기 전에 기본 값인 자동 커밋 모드로 변경하는 것이 안전함
+                // (보통은 직접 하지 않고 프레임워크가 하지만 ... 이렇게 직접 사용할 때 주의해야)
+                con.close();
+            } catch (Exception e) {
+                log.info("error", e);
+            }
+        }
+    }
+}
