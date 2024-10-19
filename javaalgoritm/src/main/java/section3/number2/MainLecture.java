90e45389cb9395c16cc45fd840df7ffc8682f4ad
yeonsuoh <lilylemonoh@naver.com>
2024-10-19 17:33:29 +0900

    섹션 3 - Array(1,2차원 배열) - 2. 보이는 학생

---
+++ javaalgoritm/src/main/java/section3/number2/MainLecture.java
@@ -0,0 +1,29 @@
+package section3.number2;
+
+import java.util.Scanner;
+
+public class MainLecture {
+    public int solution(int n, int[] arr) {
+        int answer = 1;
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) {
+                answer++;
+                max = arr[i];
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        MainLecture mainLecture = new MainLecture();
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println(mainLecture.solution(n, arr));
+    }
+}
