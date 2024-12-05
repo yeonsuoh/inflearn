ba37d780d22235beabc67995535402aa67c9f2b6
yeonsuoh <lilylemonoh@naver.com>
2024-12-05 21:52:37 +0900

    섹션 3 - Array(1,2차원 배열) - 10. 봉우리

---
+++ javaalgoritm/src/main/java/section3/number10/MainLecture.java
@@ -0,0 +1,41 @@
+package section3.number10;
+
+import java.util.Scanner;
+
+public class MainLecture {
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
+    public int solution(int n, int[][] arr) {
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean flag = true;
+                for (int k = 0; k <4; k++) {
+                    int nx = i + dx[k];
+                    int ny = j + dy[k];
+                    if (nx >= 0 && nx < n && ny >= 0 && ny < n &&
+                            arr[nx][ny] >= arr[i][j]) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        MainLecture T = new MainLecture();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+        System.out.println(T.solution(n, arr));
+    }
+}
